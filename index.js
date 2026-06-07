fe9722e57c76e43ddd13b26b7a232ddcdbb4b3fd
Elamin Fadlalla <aldeez911088@gmail.com>
2026-06-07 20:38:53 +0100

    Grabs the percent input field

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ document.getElementById("percent--form").addEventListener("submit", event => {
     event.preventDefault();
     console.log("Hello, World!");
     const amount = document.getElementById("amount").value;
-    console.log(amount);
+    const percent = document.getElementById("percent").value;
+    console.log(amount, percent);
 });
