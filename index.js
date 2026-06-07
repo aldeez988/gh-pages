d27ae6b339bd4b5645a4d5ac5a7338f0ac5f3d85
Elamin Fadlalla <aldeez911088@gmail.com>
2026-06-07 20:00:12 +0100

    Grabs the amount inout field

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 document.getElementById("percent--form").addEventListener("submit", event => {
     event.preventDefault();
     console.log("Hello, World!");
+    const amount = document.getElementById("amount").value;
+    console.log(amount);
 });
